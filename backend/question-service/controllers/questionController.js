4f795277bbdd05f8404c6badc84df072117c4cfb
Muhammad Fahim Tajwar <fahim.tajwar@u.nus.edu>
2024-09-25 08:01:04 +0800

    Add zod validation to validate request parameters on getQuestionById.

--- backend/question-service/controllers/questionController.js
+++ backend/question-service/controllers/questionController.js
@@ -1,9 +1,17 @@
+const z = require("zod");
+
 exports.getAllQuestions = async (req, res) => {
     res.send("SENT ALL QUESTIONS");
 };
 
 exports.getQuestionById = async (req, res) => {
-    res.send(`SENT QUESTION NUMBER ${req.params.questionId}`)
+    const questionIdSchema = z.number().int().positive();
+    parsedId = questionIdSchema.safeParse(Number(req.params.questionId));
+    if (!parsedId.success) {
+        res.status(400).send(`Invalid Question ID ${req.params.questionId}`)
+        return
+    } 
+    res.send(`SENT QUESTION NUMBER ${parsedId.data}`)
 };
 
 exports.dummyCallbackFunction = async (req, res) => {
