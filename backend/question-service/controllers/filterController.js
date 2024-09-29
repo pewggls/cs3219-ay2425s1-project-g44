d50a00c8edb0246fdf080add77976e96ee0f9f42
John Chia Heng Kit <e0268812@u.nus.edu>
2024-09-29 15:13:30 +0800

    Add FR3.2.3 Search by description

--- backend/question-service/controllers/filterController.js
+++ backend/question-service/controllers/filterController.js
@@ -1,20 +1,29 @@
 const Question = require("../models/question");
 
 exports.filterBy = async (req, res) => {
-    const categories = req.query.categories? req.query.categories.split(",") : undefined;
-    const complexities = req.query.complexities? req.query.complexities.split(",") : undefined;
-    if (!categories && !complexities) {
+    const categories = req.query.categories ? req.query.categories.split(",") : undefined;
+    const complexities = req.query.complexities ? req.query.complexities.split(",") : undefined;
+    const keywords = req.query.keywords ? req.query.keywords.split(",") : undefined;
+    if (!categories && !complexities && !keywords) {
         res.send("No filter found");
         return;
     }
     let query = {};
     if (categories) {
-        query.category = {$in: categories};
+        query.category = { $in: categories };
     }
 
     if (complexities) {
-        query.complexity = {$in: complexities};
+        query.complexity = { $in: complexities };
     }
+
+    if (keywords) {
+        query.$or = [
+            { title: { $regex: keywords.join('|'), $options: 'i' } },
+            { description: { $regex: keywords.join('|'), $options: 'i' } }
+        ];
+    }
+
 
     const queryResult = await Question.find(query).exec();
     if (!queryResult) {
